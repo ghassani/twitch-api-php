748ae0d3e1fd9d1594a54fb74bab6decb8668192
Gassan Idriss <ghassani@hmail.com>
2016-03-18 12:00:04 -0700

    tabs to spaces

--- authenticator/index.php
+++ authenticator/index.php
@@ -11,31 +11,31 @@ session_start();
 $authenticator = new Authenticator(TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET, TWITCH_REDIRECT_URI);
 
 $authenticator->setScope(array(
-	'user_read',
-	'user_blocks_edit',
-	'user_blocks_read',
-	'user_follows_edit',
-	'channel_read',
-	'channel_editor',
-	'channel_commercial',
-	'channel_stream',
-	'channel_subscriptions',
-	'user_subscriptions',
-	'channel_check_subscription',
-	'chat_login',
+    'user_read',
+    'user_blocks_edit',
+    'user_blocks_read',
+    'user_follows_edit',
+    'channel_read',
+    'channel_editor',
+    'channel_commercial',
+    'channel_stream',
+    'channel_subscriptions',
+    'user_subscriptions',
+    'channel_check_subscription',
+    'chat_login',
 ));
 
 $code = isset($_REQUEST['code']) ? $_REQUEST['code'] : null;
 
 if (!$code) {
-	header(sprintf('Location: %s', $authenticator->getAuthorizeUrl()));
-	exit;
+    header(sprintf('Location: %s', $authenticator->getAuthorizeUrl()));
+    exit;
 } else {
-	try {
-		$tokenResponse = $authenticator->getToken($code);
-	} catch (\Exception $e) {
-		$error = $e->getMessage();
-	}
+    try {
+        $tokenResponse = $authenticator->getToken($code);
+    } catch (\Exception $e) {
+        $error = $e->getMessage();
+    }
 }
 
 ?>
@@ -49,25 +49,25 @@ if (!$code) {
   <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
 </head>
-	<div>
-		<a href="/index.php">Authorize</a>
-	</div>
-	<hr />
-	<div id="access-token">
-		<?php if (isset($error)): ?>
-			<div style="color:red;"><?php echo $error; ?></div>
-		<?php else:?>
-			<div>Access Token: <?php echo $tokenResponse['access_token']; ?></div>
-			<div>Refresh Token: <?php echo $tokenResponse['refresh_token']; ?></div>
-			<div>Scopes:
-				<ul>
-					<?php foreach ($tokenResponse['scope'] as $scope): ?>
-						<li><?php echo $scope; ?></li>
-					<?php endforeach; ?>
-				</ul>
-			</div>
-		<?php endif;?>
-	</div>
+    <div>
+        <a href="/index.php">Authorize</a>
+    </div>
+    <hr />
+    <div id="access-token">
+        <?php if (isset($error)): ?>
+            <div style="color:red;"><?php echo $error; ?></div>
+        <?php else:?>
+            <div>Access Token: <?php echo $tokenResponse['access_token']; ?></div>
+            <div>Refresh Token: <?php echo $tokenResponse['refresh_token']; ?></div>
+            <div>Scopes:
+                <ul>
+                    <?php foreach ($tokenResponse['scope'] as $scope): ?>
+                        <li><?php echo $scope; ?></li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        <?php endif;?>
+    </div>
 <body>
 </body>
 </html>
--- src/Authenticator.php
+++ src/Authenticator.php
@@ -21,245 +21,245 @@ use GuzzleHttp\Client as HttpClient;
 class Authenticator
 {
 
-	const ENDPOINT = 'https://api.twitch.tv/kraken/oauth2/';
+    const ENDPOINT = 'https://api.twitch.tv/kraken/oauth2/';
 
-	protected $clientId;
+    protected $clientId;
 
-	protected $clientSecret;
+    protected $clientSecret;
 
-	protected $redirectUri;
+    protected $redirectUri;
 
-	protected $state;
+    protected $state;
 
-	protected $scope = array();
+    protected $scope = array();
 
-	protected $forceVerify = false;
+    protected $forceVerify = false;
 
-	private $httpClient;
+    private $httpClient;
 
-	private $availableScopes = array(
-		'user_read',
-		'user_blocks_edit',
-		'user_blocks_read',
-		'user_follows_edit',
-		'channel_read',
-		'channel_editor',
-		'channel_commercial',
-		'channel_stream',
-		'channel_subscriptions',
-		'user_subscriptions',
-		'channel_check_subscription',
-		'chat_login',
-	);
+    private $availableScopes = array(
+        'user_read',
+        'user_blocks_edit',
+        'user_blocks_read',
+        'user_follows_edit',
+        'channel_read',
+        'channel_editor',
+        'channel_commercial',
+        'channel_stream',
+        'channel_subscriptions',
+        'user_subscriptions',
+        'channel_check_subscription',
+        'chat_login',
+    );
 
-	/**
-	* Constructor
-	*/
-	public function __construct($clientId, $clientSecret, $redirectUri, array $scope = array())
-	{
-		$this->clientId = $clientId;
-		$this->clientSecret = $clientSecret;
-		$this->redirectUri = $redirectUri;
-		$this->scope = $scope;
+    /**
+    * Constructor
+    */
+    public function __construct($clientId, $clientSecret, $redirectUri, array $scope = array())
+    {
+        $this->clientId = $clientId;
+        $this->clientSecret = $clientSecret;
+        $this->redirectUri = $redirectUri;
+        $this->scope = $scope;
 
-		$this->httpClient = new HttpClient(array('base_url' => static::ENDPOINT));
-	}
+        $this->httpClient = new HttpClient(array('base_url' => static::ENDPOINT));
+    }
 
-	/**
-	* getClientId
-	*
-	* @return string
-	*/
-	public function getClientId()
-	{
-		return $this->clientId;
-	}
+    /**
+    * getClientId
+    *
+    * @return string
+    */
+    public function getClientId()
+    {
+        return $this->clientId;
+    }
 
-	/**
-	* setClientId - Set the client id to authenticate against
-	*
-	* @param string $clientId
-	* @return Authenticator
-	*/
-	public function setClientId($clientId)
-	{
-		$this->clientId = $clientId;
-		return $this;
-	}
+    /**
+    * setClientId - Set the client id to authenticate against
+    *
+    * @param string $clientId
+    * @return Authenticator
+    */
+    public function setClientId($clientId)
+    {
+        $this->clientId = $clientId;
+        return $this;
+    }
 
-	/**
-	* getClientSecret
-	*
-	* @return string
-	*/
-	public function getClientSecret()
-	{
-		return $this->clientSecret;
-	}
+    /**
+    * getClientSecret
+    *
+    * @return string
+    */
+    public function getClientSecret()
+    {
+        return $this->clientSecret;
+    }
 
-	/**
-	* setClientSecret - Set the client secret
-	*
-	* @param string $clientSecret
-	* @return Authenticator
-	*/
-	public function setClientSecret($clientId)
-	{
-		$this->clientSecret = $clientSecret;
-		return $this;
-	}
+    /**
+    * setClientSecret - Set the client secret
+    *
+    * @param string $clientSecret
+    * @return Authenticator
+    */
+    public function setClientSecret($clientId)
+    {
+        $this->clientSecret = $clientSecret;
+        return $this;
+    }
 
-	/**
-	* getRedirectUri
-	*
-	* @return string
-	*/
-	public function getRedirectUri()
-	{
-		return $this->redirectUri;
-	}
+    /**
+    * getRedirectUri
+    *
+    * @return string
+    */
+    public function getRedirectUri()
+    {
+        return $this->redirectUri;
+    }
 
-	/**
-	* setRedirectUri - Set the redirect uri
-	*
-	* @param string $redirectUri
-	* @return Authenticator
-	*/
-	public function setRedirectUri($redirectUri)
-	{
-		$this->redirectUri = $redirectUri;
-		return $this;
-	}
+    /**
+    * setRedirectUri - Set the redirect uri
+    *
+    * @param string $redirectUri
+    * @return Authenticator
+    */
+    public function setRedirectUri($redirectUri)
+    {
+        $this->redirectUri = $redirectUri;
+        return $this;
+    }
 
-	/**
-	* getState
-	*
-	* @return string
-	*/
-	public function getState()
-	{
-		return $this->state;
-	}
+    /**
+    * getState
+    *
+    * @return string
+    */
+    public function getState()
+    {
+        return $this->state;
+    }
 
-	/**
-	* setState - Set the state anti-csrf parameter
-	*
-	* @param string $state
-	* @return Authenticator
-	*/
-	public function setState($state)
-	{
-		$this->state = $state;
-		return $this;
-	}
+    /**
+    * setState - Set the state anti-csrf parameter
+    *
+    * @param string $state
+    * @return Authenticator
+    */
+    public function setState($state)
+    {
+        $this->state = $state;
+        return $this;
+    }
 
 
-	/**
-	* setScope - Set the scope, overwriting existing values with the passed value(s)
-	*
-	* @param string|array $scope 
-	* @throws InvalidArgumentException - When a passed scope is invalid
-	* @return Authenticator
-	*/
-	public function setScope($scope)
-	{
-		if (is_array($scope)) {
-			foreach ($scope as $s) {
-				$this->validateScope($s);
-			}
-			$this->scope = $scope;
-		} else {
-			$this->validateScope($scope);
-			$this->scope = array($scope);
-		}
+    /**
+    * setScope - Set the scope, overwriting existing values with the passed value(s)
+    *
+    * @param string|array $scope 
+    * @throws InvalidArgumentException - When a passed scope is invalid
+    * @return Authenticator
+    */
+    public function setScope($scope)
+    {
+        if (is_array($scope)) {
+            foreach ($scope as $s) {
+                $this->validateScope($s);
+            }
+            $this->scope = $scope;
+        } else {
+            $this->validateScope($scope);
+            $this->scope = array($scope);
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	* getScope - Retrieve the scope stack
-	*
-	* @return array
-	*/
-	public function getScope()
-	{
-		return $this->scope;
-	}
+    /**
+    * getScope - Retrieve the scope stack
+    *
+    * @return array
+    */
+    public function getScope()
+    {
+        return $this->scope;
+    }
 
-	/**
-	* addScope - Add a scope into the scope stack
-	*
-	* @param string|array $scope
-	* @throws InvalidArgumentException - When a passed scope is invalid
-	* @return Authenticator
-	*/
-	public function addScope($scope)
-	{
-		if (is_array($scope)) {
-			foreach ($scope as $s) {
-				$this->validateScope($s);
-				array_push($this->scope, $s);
-			}
-		} else {
-			$this->validateScope($scope);
-			array_push($this->scope, $scope);
-		}
+    /**
+    * addScope - Add a scope into the scope stack
+    *
+    * @param string|array $scope
+    * @throws InvalidArgumentException - When a passed scope is invalid
+    * @return Authenticator
+    */
+    public function addScope($scope)
+    {
+        if (is_array($scope)) {
+            foreach ($scope as $s) {
+                $this->validateScope($s);
+                array_push($this->scope, $s);
+            }
+        } else {
+            $this->validateScope($scope);
+            array_push($this->scope, $scope);
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	* validateScope - Validate that a passed scope is valid
-	*
-	* @param string|array $scope
-	* @throws InvalidArgumentException - When a passed scope is invalid
-	* @return void
-	*/
-	private function validateScope($scope)
-	{
-		if (!in_array($scope, $this->availableScopes)) {
-			throw new \InvalidArgumentException(sprintf('Scope %s is invalid. Available scopes: %s',
-				$s, implode(', ', $this->availableScopes)
-			));
-		}
-	}
+    /**
+    * validateScope - Validate that a passed scope is valid
+    *
+    * @param string|array $scope
+    * @throws InvalidArgumentException - When a passed scope is invalid
+    * @return void
+    */
+    private function validateScope($scope)
+    {
+        if (!in_array($scope, $this->availableScopes)) {
+            throw new \InvalidArgumentException(sprintf('Scope %s is invalid. Available scopes: %s',
+                $s, implode(', ', $this->availableScopes)
+            ));
+        }
+    }
 
-	/**
-	 * getAuthorizeUrl - The URL to redirect the user to get an authorization code
-	 * 
-	 * @return string
-	 */ 
-	public function getAuthorizeUrl()
-	{
-		return sprintf('%sauthorize?%s', static::ENDPOINT, http_build_query(array(
-			'client_id' 	=> $this->getClientId(),
-			'redirect_uri'  => $this->getRedirectUri(),
-			'state'			=> $this->getState(),
-			'scope'			=> implode(' ', $this->scope),
-			'response_type' => 'code'
-		)));
-	}
+    /**
+     * getAuthorizeUrl - The URL to redirect the user to get an authorization code
+     * 
+     * @return string
+     */ 
+    public function getAuthorizeUrl()
+    {
+        return sprintf('%sauthorize?%s', static::ENDPOINT, http_build_query(array(
+            'client_id'     => $this->getClientId(),
+            'redirect_uri'  => $this->getRedirectUri(),
+            'state'         => $this->getState(),
+            'scope'         => implode(' ', $this->scope),
+            'response_type' => 'code'
+        )));
+    }
 
-	/**
-	* getToken - Exchange the authorization code for an authorization token
-	*
-	* @param string $code 
-	* @return array
-	*/
-	public function getToken($code)
-	{
-		$request = $this->httpClient->createRequest('POST', 'token', array(
-				'body' => array(
-					'client_id'		=> $this->getClientId(),
-					'client_secret'	=> $this->getClientSecret(),
-					'state'			=> $this->getState(),
-					'grant_type'	=> 'authorization_code',
-					'redirect_uri'	=> $this->getRedirectUri(),
-					'code'			=> $code
-				)
-			)					
-		);
+    /**
+    * getToken - Exchange the authorization code for an authorization token
+    *
+    * @param string $code 
+    * @return array
+    */
+    public function getToken($code)
+    {
+        $request = $this->httpClient->createRequest('POST', 'token', array(
+                'body' => array(
+                    'client_id'     => $this->getClientId(),
+                    'client_secret' => $this->getClientSecret(),
+                    'state'         => $this->getState(),
+                    'grant_type'    => 'authorization_code',
+                    'redirect_uri'  => $this->getRedirectUri(),
+                    'code'          => $code
+                )
+            )                   
+        );
 
-		return $this->httpClient->send($request)->json();
-	}
+        return $this->httpClient->send($request)->json();
+    }
 }
